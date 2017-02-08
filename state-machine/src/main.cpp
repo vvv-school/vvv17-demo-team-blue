d7de9745330636459f8c9cdbede02c759ee2dde1
Nolwenn Briquet <nolwenn.briquet-kerestedjian@centralesupelec.fr>
2017-02-08 03:56:51 +0100

    changes in ports and state-machine

--- state-machine/src/main.cpp
+++ state-machine/src/main.cpp
@@ -26,6 +26,8 @@ protected:
 
     RpcServer rpcPort;
     yarp::os::BufferedPort<yarp::os::Bottle> stateOutPort;
+    yarp::os::BufferedPort<yarp::os::Bottle> gazeInport;
+    yarp::os::BufferedPort<yarp::os::Bottle> cardInport;
 
     bool                        closing;
 
@@ -41,22 +43,27 @@ public:
     {
         string robot=rf.check("robot",Value("icubSim")).asString();
 
-        bool ret = true;
 
-        ret &= stateOutPort.open("/state-machine/state:o");
-        if(!ret) {
-            yError()<<"Cannot open some of the ports";
+        if(!stateOutPort.open("/state-machine/state:o")) {
+            yError()<<"Cannot open the stateOutPort";
+            return false;
+        }
+
+        if(!gazeInport.open("/state-machine/look:i")) {
+            yError()<<"Cannot open the gazeInport";
+            return false;
+        }
+
+        if(!cardInport.open("/state-machine/card:i")) {
+            yError()<<"Cannot open the cardInport";
             return false;
         }
 
         rpcPort.open("/state-machine/command");
         attach(rpcPort);
 
-
         int score_robot = 0 ;
         int score_human = 0 ;
-
-        bool ok_look_down = false ;
 
         return true;
     }
@@ -73,42 +80,55 @@ public:
         }
         else if (cmd=="start")
         {
+            Bottle *gaze_input = gazeInport.read();
+            int look_down = gaze_input->get(0).asInt();
+
+            Bottle *card_input = cardInport.read();
+
+            int ind_1 = card_input->get(0).asInt();
+            int x_1 = card_input->get(1).asInt();
+            int y_1 = card_input->get(2).asInt();
+            int ind_2 = card_input->get(3).asInt();
+            int x_2 = card_input->get(4).asInt();
+            int y_2 = card_input->get(5).asInt();
+
+            // Let's start !
+
             publishState("starting");
 
             //we delay a bit so that the robot can speak & ppl can marvel at it
             yarp::os::Time::delay(3.0);
-            ok_look_down = true;
+            publishState("look down");
 
-            look_down(); // ok_look_down has to be tuned true
-            publishState("looking at cards");
-
-            cardRecognition() ; // update both scores inside
+            if (look_down){
+              publishState("looking at cards");
+              updateScore(ind_1,ind_2) ; // update both scores inside
+            }
+            else {
+              publishState("look down");
+            }
 
             if (score_robot > score_human){
                 publishState("bet");
-                pushObject() ;
             }
             else {
                 publishState("don't bet");
             }
 
-
-            // The dealer distributes cards
-            if (ok_look_down){
-                look_up() ; // ok_look_down has to be tuned false
-                Time::delay(2.0);
+            if (look_down){
                 publishState("look up");
             }
 
-            if (!ok_look_down){
-                look_down() ; // ok_look_down has to be tuned true
+            // The dealer distributes cards in the meantime
+            Time::delay(2.0);
+
+            if (!look_down){
                 publishState("looking at cards");
+                updateScore(ind_1, ind_2) ;  // update both scores inside
             }
 
-            cardRecognition() ;  // update both scores inside
-
-            if (score_robot > score_human){
-                pullObject() ;
+        if (score_robot > score_human){
+                publishState("pull object");
                 publishState("won");
             }
             else
@@ -122,51 +142,35 @@ public:
         return true;
     }
 
-    void pullObject()
-    {
-        // FILL IN HERE
-    }
+    /***************************************************/
 
-    void cardRecognition()
+    void updateScore(int ind_robot, int ind_human)
     {
         // FILL IN HERE
         // check that we have card data
         // set
 
-
+        score_robot = ind_robot ;
+        score_human = ind_human ;
     }
-
-    void look_down()
-    {
-        // FILL IN HERE
-        ok_look_down = true;
-    }
-
-    void look_up()
-    {
-        // FILL IN HERE
-    }
-
-
-    void pushObject()
-    {
-        // FILL IN HERE
-    }
-
 
 
     /***************************************************/
     bool interrupt()
     {
+        stateOutPort.interrupt();
+        gazeInport.interrupt();
+        cardInport.interrupt();
         return true;
     }
 
     /**********************************************************/
     bool close()
     {
-        // we have to close ports here
         rpcPort.close();
         stateOutPort.close();
+        gazeInport.close();
+        cardInport.close();
         return true;
     }
 
@@ -187,6 +191,8 @@ public:
     {
         return !closing;
     }
+
+    /***************************************************/
 
     void publishState(const std::string& state)
     {
